e736f6d2009d3f0689945d3f71c8234ebcade693
walla2j9 <walla2j9@ucmail.uc.edu>
2026-06-15 17:39:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ShipCostCalculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double itemPrice = 0.0;
+        double shippingCost = 0.0;
+        double totalPrice = 0.0;
+        String invalidInput = "";
+
+        System.out.print("Please enter the price of the item: $");
+
+        if (in.hasNextDouble()) {
+            itemPrice = in.nextDouble();
+            in.nextLine();
+
+            if (itemPrice >= 100.0) {
+                shippingCost = 0.0;
+            } else {
+                shippingCost = itemPrice * 0.02;
+            }
+            totalPrice = itemPrice + shippingCost;
+            System.out.println("Shipping Cost: $" + shippingCost);
+            System.out.println("Total Price: $" + totalPrice);
+        } else {
+            invalidInput = in.nextLine();
+            System.out.println("Invalid input: " + invalidInput);
+            System.out.println("Run the program again and enter a valid numeric amount!");
+        }
+    }
+}
